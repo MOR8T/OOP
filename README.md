fb0444e91672eae5467c04f2db5bc5352b4d8976
Shukurov Shahrom <112476380+MOR8T@users.noreply.github.com>
2022-10-14 15:12:30 +0500

    commit2

---
+++ README.md
@@ -0,0 +1,7 @@
+# OOP
+#
+## Encapsulation
+#### Encapsulation is the bundling of data and the methods that act on that data such that access to that data is restricted from outside the bundle, or as Alan Kay describes it, “local retention and protection and hiding of state-process.” In OOP, that means that an object stores its state privately, and only the object’s methods have access to change it.
+![](./img/img.jpeg)
+#
+## 
---
+++ main.js
@@ -0,0 +1,23 @@
+// #1
+// class MobilePhone{
+//     constructor(name,aperativememory,memory,color,price){
+//         this.name = name;
+//         this.aperativememory = aperativememory;
+//         this.memory = memory;
+//         this.color = color;
+//         this.price = price;
+//     }
+//     MyPhone(){
+//         return `My phono\`s name ${this.name} his price ${this.price}`;
+//     }
+// }
+// class Xiaume extends MobilePhone {
+//     constructor(name,aperativememory,memory,color,price,model,camera){
+//         super(name,aperativememory,memory,color,price)
+//             this.model = model;
+//             this.camera = camera;
+//     }
+// }
+// let X11lite = new Xiaume("Xiaume","8GB","512GB","black",3000,"11 lite NE","64MP");
+// console.log(X11lite);
+// console.log(X11lite.MyPhone());
